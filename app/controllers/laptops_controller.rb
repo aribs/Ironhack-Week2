de7fe5b60d906a698c75424f276989e5662c76c7
Alejo Ribes <alejoribes@gmail.com>
2015-04-16 17:40:17 +0200

    Add new Laptop and browsing links added

--- app/controllers/laptops_controller.rb
+++ app/controllers/laptops_controller.rb
@@ -5,4 +5,16 @@ class LaptopsController < ApplicationController
 	def show
 		@laptops = Laptop.find(params[:id])
 	end
+	def new
+		@laptop = Laptop.new
+	end
+	def create
+		@laptop = Laptop.new(laptop_params)
+		@laptop.save
+		redirect_to laptops_path(@laptop)
+	end
+	private
+	def laptop_params
+		params.require(:laptop).permit(:brand, :model, :processorBrand, :ram, :price )
+	end
 end
