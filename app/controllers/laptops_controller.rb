e743ede4ce00a3d9b1c3d82e8d16da31daa46328
Alejo Ribes <alejoribes@gmail.com>
2015-04-17 16:37:28 +0200

    First Iteration Exercise Finished!

--- app/controllers/laptops_controller.rb
+++ app/controllers/laptops_controller.rb
@@ -13,6 +13,20 @@ class LaptopsController < ApplicationController
 		@laptop.save
 		redirect_to laptops_path(@laptop)
 	end
+	def update
+		@laptop = Laptop.find(params[:id])
+		@laptop.update(laptop_params)
+		redirect_to laptop_path(@laptop)
+	end
+	def edit 
+		@laptop = Laptop.find(params[:id])
+
+	end
+	def destroy
+		@laptop = Laptop.find(params[:id])
+		@laptop.destroy
+		redirect_to root_path
+	end
 	private
 	def laptop_params
 		params.require(:laptop).permit(:brand, :model, :processorBrand, :ram, :price )
